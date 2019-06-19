a21b5040ac12d1788cf544d5b717ff0e5a1b1d18
Rohit Pai <rodude123@gmail.com>
2019-06-20 00:45:36 +0100

    Updated Quiz and GUI
    Did a lot of thins! Very happy with the progress!
    
    Renamed Quiz class QuizConsole because it is the console version of the game.
    Created a new Quiz class to house all methods required for the GUI.
    Updated GUI to include quiz game GUI no functionality yet for quiz so not playable yet i.e. not events for the game

--- src/com/quiz/quiz/GUI.java
+++ src/com/quiz/quiz/GUI.java
@@ -3,9 +3,11 @@ package com.quiz.quiz;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.omg.PortableServer.POA;
@@ -14,25 +16,41 @@ public class GUI extends Application
 {
 
 	private Stage primaryStage;
+	private Quiz quizGame;
+	private int nxt;
 
 	@Override
 	public void start(Stage primaryStage)
 	{
 		this.primaryStage = primaryStage;
+		this.quizGame = new Quiz();
+		nxt = 1;
 		menu();
 	}
 
 	private void menu()
 	{
 		VBox root = new VBox();
-		Label title = new Label("QuizConsole");
+		Label title = new Label("Quiz");
 		title.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
-		Button btnQuiz = new Button("QuizConsole");
+		Button btnQuiz = new Button("Quiz");
 		btnQuiz.setOnAction(event -> quiz());
+		btnQuiz.setMaxWidth(75);
 		Button btnCreate = new Button("Create");
+		btnCreate.setOnAction(event -> create());
+		btnCreate.setMaxWidth(75);
 		Button btnQuit = new Button("Quit");
 		btnQuit.setOnAction(event -> System.exit(0));
-		root.getChildren().addAll(title, btnQuiz, btnCreate, btnQuit);
+		btnQuit.setMaxWidth(75);
+		baseGUI(root, new Node[] {title, btnQuiz, btnCreate, btnQuit});
+	}
+
+	private void baseGUI(VBox root, Node[] nodes)
+	{
+		for (Node node : nodes)
+		{
+			root.getChildren().add(node);
+		}
 		root.setPadding(new Insets(10));
 		root.setAlignment(Pos.CENTER);
 		root.setSpacing(10.00);
@@ -44,10 +62,22 @@ public class GUI extends Application
 	private void quiz()
 	{
 		VBox root = new VBox();
+		Label title = new Label("Question " + nxt);
+		title.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
+		title.setPadding(new Insets(10,0,10,0));
+		Label qLabel = new Label(quizGame.getNextQuestion(nxt));
+		qLabel.setPadding(new Insets(10,0,10,0));
+		TextField answerField = new TextField();
+		answerField.setAlignment(Pos.CENTER);
+		answerField.setMaxWidth(100);
+		Button submitBtn = new Button("Submit");
+		submitBtn.setMaxWidth(100);
+		baseGUI(root, new Node[] { title, qLabel, answerField, submitBtn});
+	}
 
-		Scene scene = new Scene(root, 250, 250);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+	private void create()
+	{
+
 	}
 
 	public static void main(String[] args)
--- src/com/quiz/quiz/Quiz.java
+++ src/com/quiz/quiz/Quiz.java
@@ -1,5 +1,53 @@
 package com.quiz.quiz;
 
+import java.util.Random;
+
+/**
+ * New Quiz class to be used in the GUI
+ */
 public class Quiz
 {
+	private String[] operator = {"+", "-", "x", "%"};
+	private Random random = new Random();
+	private int firstNo = 0;
+	private int secondNo = 0;
+
+	public String getNextQuestion(int nxt)
+	{
+		int firstNo = random.nextInt(20);
+		int secondNo = random.nextInt(20);
+		int i = random.nextInt(operator.length);
+
+		if (operator[i].equals("%"))
+		{
+			secondNo += 1;
+		}
+
+		this.firstNo = firstNo;
+		this.secondNo = secondNo;
+
+		return "What is " + firstNo + " " + operator[i] + " " + secondNo + " = ";
+	}
+
+	public boolean checkAnswer(int inputAnswer, int nxt)
+	{
+		int answer = 0;
+		switch (operator[nxt])
+		{
+			case "+":
+				answer = (firstNo + secondNo);
+				break;
+			case "-":
+				answer = (firstNo - secondNo);
+				break;
+			case "x":
+				answer = (firstNo * secondNo);
+				break;
+			case "%":
+				answer = (firstNo % secondNo);
+				break;
+		}
+
+		return inputAnswer == answer;
+	}
 }
